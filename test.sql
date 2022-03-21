c7ad897fd0fabcb945d179efc508cab3228c1261
unknown <olenalialyuk@gmail.com>
2022-03-22 00:02:28 +0200

    tables Employee and Employer are done

--- test.sql
+++ test.sql
@@ -13,3 +13,50 @@ name VARCHAR(20) NOT NULL,
 surname VARCHAR(20) NOT NULL,
 team VARCHAR(20) NOT NULL,
 FOREIGN KEY (id) references employee(id));
+INSERT INTO employee (name, surname, team) value ("Aashvi", "Divisha", "Rutvi", "Aadvika," "Kayra", "Saanvi", "Shreyanvi", "Charlotte", "Livie", "Olivia", "Sophie", "Saffi", "Jones", "Davis", "Johnson", "Williams", "Smith", "Garcia", "Rodriguez", "Brown", "Miller", "Martinez", "Lopez", "Hernandez", "QA", "Dev", "PM", "PR", "Sales", "Analyst", "PM", "QA", "PR", "Dev", "Dev", "Dev");
+SHOW TABLES;
+INSERT INTO employee (Name, surname, team) value (Aashvi, Jones, QA);
+INSERT INTO employee (name, surname, team) value ("Aashvi", "Divisha", "Rutvi", "Aadvika," "Kayra", "Saanvi", "Shreyanvi", "Charlotte", "Livie", "Olivia", "Sophie", "Saffi", "Jones", "Davis", "Johnson", "Williams", "Smith", "Garcia", "Rodriguez", "Brown", "Miller", "Martinez", "Lopez", "Hernandez", "QA", "Dev", "PM", "PR", "Sales", "Analyst", "PM", "QA", "PR", "Dev", "Dev", "Dev");
+INSERT INTO employee (name, surname, team) value ("Aashvi", "Jones", "QA");
+INSERT INTO employee (name, surname, team) value ("Divisha",  "Davis", "QA");
+INSERT INTO employee (name, surname, team) value ("Rutvi",   "Johnson",  "Dev");
+INSERT INTO employee (name, surname, team) value ( "Aadvika", "Johnson", "PM");
+SELECT * FROM employee;
+UPDATE employee SET Name="Mary" WHERE id=1;
+UPDATE employee SET Name="Anna" WHERE id=4;
+SELECT * FROM employee;
+UPDATE employee SET Name="Nadia" WHERE id=7;
+UPDATE employee SET Name="Olivia" WHERE id=8;
+UPDATE employee SET Name="Emma" WHERE id=9;
+UPDATE employee SET Name="Charlotte" WHERE id=10;
+UPDATE employee SET surname="Elsher" WHERE id=3;
+UPDATE employee SET surname="Solace" WHERE id=4;
+UPDATE employee SET surname="Levine" WHERE id=5;
+UPDATE employee SET surname="Raven" WHERE id=7;
+UPDATE employee SET surname="Bardot" WHERE id=8;
+UPDATE employee SET surname="Hansley" WHERE id=9;
+INSERT INTO employer (Name, surname, team, id) value (6,"Aashvi", "Jones", "QA", 8 );
+SELECT* FROM employer;
+DROP TABLE employer;
+CREATE TABLE employer
+(id INT AUTO_INCREMENT PRIMARY KEY,
+employee_id INT NOT NULL,
+name VARCHAR(20) NOT NULL,
+surname VARCHAR(20) NOT NULL,
+team VARCHAR(20) NOT NULL,
+FOREIGN KEY (employee_id) references employee(id));
+INSERT INTO employer (employee_id, name, surname, team) value (1, "Emilia", "Rojas", "QA");
+INSERT INTO employer (employee_id, name, surname, team) value (2, "Emilia", "Rojas", "QA");
+INSERT INTO employer (employee_id, name, surname, team) value (3, "Emilia", "Rojas", "QA");
+INSERT INTO employer (employee_id, name, surname, team) value (4, "Emilia", "Rojas", "QA");
+INSERT INTO employer (employee_id, name, surname, team) value (7, "Emilia", "Rojas", "QA");
+INSERT INTO employer (employee_id, name, surname, team) value (8, "Emilia", "Rojas", "QA");
+INSERT INTO employer (employee_id, name, surname, team) value (5, "Zoe", "Yancy", "dev");
+INSERT INTO employer (employee_id, name, surname, team) value (9, "Zoe", "Yancy", "dev");
+INSERT INTO employer (employee_id, name, surname, team) value (6, "Everly", "Winslet", "PM"); 
+INSERT INTO employer (employee_id, name, surname, team) value (10, "Everly", "Winslet", "PM");
+INSERT INTO employer (employee_id, name, surname, team) value (12, "Leah", "Quinn", "Sales"); 
+INSERT INTO employer (employee_id, name, surname, team) value (11, "Lillian", "Kinle", "PR"); 
+INSERT INTO employer (employee_id, name, surname, team) value (13, "Theodore ", "Seuss", "Analyst");
+SELECT* FROM employer;
+SELECT* FROM employee;
